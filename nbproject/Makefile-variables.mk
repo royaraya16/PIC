503d4f7f2dfcdf84835d051ccaf08ea7a77418b1
roy <roy@Tesla>
2015-08-29 23:17:54 -0600

    prueba1main

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=GIT.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/GIT.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=git.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/git.x.tar
